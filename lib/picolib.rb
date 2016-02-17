a081d450efae9907b73645c0bbc08ed905396cfc
elvis <elvis@u3dspace.com>
2016-02-17 22:23:53 +0800

    Support decode and encode pico_sign

--- lib/picolib.rb
+++ lib/picolib.rb
@@ -6,6 +6,7 @@ require "picolib/app_api"
 require "picolib/client_api"
 require "picolib/contact_api"
 require "picolib/oauth2_api"
+require "picolib/pico_sign"
 
 # HTTP module
 require "picolib/http_service"
@@ -17,4 +18,12 @@ module Picolib
   def self.http_request(path, args, verb, options = {})
     HTTPService.request(path, args, verb, options)
   end
+
+  def self.decode_pico_sign(pico_sign, client_secret)
+  	PicoSign.decode_signed_request(pico_sign, client_secret)
+  end
+
+  def self.encode_pico_sign(payload, client_secret)
+  	PicoSign.encode_signed_request(payload, client_secret)
+  end  
 end
---
+++ lib/picolib/pico_sign.rb
@@ -0,0 +1,23 @@
+require "Base64"
+
+module Picolib
+  module PicoSign
+    def self.decode_signed_request(signed_request, client_secret)
+      encoded_signature, encoded_data = signed_request.split('.')
+      signature = Base64.decode64(encoded_signature)
+      expected_signature = OpenSSL::HMAC::digest('sha256', client_secret, encoded_data)
+      if signature == expected_signature
+        JSON.parse Base64.decode64(encoded_data)
+      else
+        false
+      end
+    end
+
+    def self.encode_signed_request(payload, client_secret)
+      encoded_data = Base64.encode64(payload)
+      signature = OpenSSL::HMAC::digest('sha256', client_secret, encoded_data)
+      encoded_signature = Base64.encode64(signature)
+      [encoded_signature, encoded_data].join('.')
+    end
+  end
+end
