efb7a7a8b43e848b8eb468ac1a1455b552f41a79
U3D <elvis@u3dspace.com>
2016-12-13 15:21:04 +0800

    Modify initial arguments

--- lib/picolib/client_api.rb
+++ lib/picolib/client_api.rb
@@ -4,23 +4,14 @@ require "uri"
 module Picolib
   module Client
     class API
-      def initialize(attributes)
-        @end_point = attributes[:end_point]
-        @headers = attributes[:headers]
-        @debug = attributes[:debug]
-        @sign = attributes[:sign]
-        @user_id = attributes[:user_id]
-        if @sign && @user_id
-          @args = {
-            headers: {
-              sign: @sign,
-              user_id: @user_id
-            }
-          }
-        elsif @headers
-          @args = {
-            headers: @headers
-          }
+      def initialize(end_point, headers=nil, opts={})
+        @end_point = end_point
+        @debug = opts[:debug]
+        
+        if headers.class == String
+          @args = {headers: {authorization: headers}}  
+        elsif headers.class == Hash 
+          @args = {headers: headers}
         else
           @args = {}
         end
